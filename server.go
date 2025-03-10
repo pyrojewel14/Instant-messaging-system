c62b0ee5265caeb7ca8dfacca97646573aae89bb
pyrojewel14 <maomuding@gmail.com>
2025-03-10 22:01:13 +0800

    basic server func

---
+++ server.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+type Server struct {
+	Ip   string
+	Port int
+}
+
+// 创建一个server的接口
+func NewServer(ip string, port int) *Server {
+	server := &Server{
+		Ip:   ip,
+		Port: port,
+	}
+	return server
+}
+
+func (s *Server) Handler(conn net.Conn) {
+	fmt.Println("connect success")
+}
+
+// 启动服务器的接口
+func (s *Server) Start() {
+	// socekt listen
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", s.Ip, s.Port))
+	if err != nil {
+		fmt.Println("net.Listen err:", err)
+		return
+	}
+
+	// close listen socket
+	defer listener.Close()
+
+	for {
+		// accept
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("listener.Accept err:", err)
+			continue
+		}
+
+		// do handler
+		go s.Handler(conn)
+	}
+
+}
